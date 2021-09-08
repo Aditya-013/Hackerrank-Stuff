939aa9d7d5665ef42c768466f13d410a23f983ab
Aditya-013 <33128013+Aditya-013@users.noreply.github.com>
2021-09-08 19:44:51 +0530

    Update Connected_Cells.cpp

--- Search/Connected_Cells.cpp
+++ Search/Connected_Cells.cpp
@@ -1,15 +1,20 @@
 #include<iostream>
 #include<climits>
+#include<cstring>
 int n, m;
-int CountVals(int* arr, int row, int col, int m){
+int t[100][100];
+int CountVals(int* arr, int row, int col){
     if(row < 0 || row > (n-1) || col<0 || col > (m-1) || *(arr + row*m +col) == -1 || *(arr + row*m +col) == 0){
         return 0;
     }
+    if(t[row][col] != -1){
+        return t[row][col];
+    }
     else{
         *(arr + row*m +col) = -1;
-        return 1 + CountVals(arr, row+1, col+1, m) + CountVals(arr, row, col+1, m) + CountVals(arr, row-1, col+1, m) + 
-                    CountVals(arr, row+1, col-1, m) + CountVals(arr, row, col-1, m) + CountVals(arr, row-1, col-1, m) + 
-                    CountVals(arr, row-1, col, m) + CountVals(arr, row+1, col, m);
+        return t[row][col] = 1 + CountVals(arr, row+1, col+1) + CountVals(arr, row, col+1) + CountVals(arr, row-1, col+1) + 
+                    CountVals(arr, row+1, col-1) + CountVals(arr, row, col-1) + CountVals(arr, row-1, col-1) + 
+                    CountVals(arr, row-1, col) + CountVals(arr, row+1, col);
     }
 }
 
@@ -18,7 +23,7 @@ int FuncOne(int* arr, int n, int m){
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
             if(*(arr + i*m +j) == 1){
-                temp = CountVals((int *)arr, i, j, m);
+                temp = CountVals((int *)arr, i, j);
                 min = std::max(min, temp);
             }
         }
@@ -26,10 +31,11 @@ int FuncOne(int* arr, int n, int m){
     return min;
 }
 int main(){
-    // n = 5;
-    // m = 5;
-    cin>>n>>m;
-    int arr[n][m];// = {{1,1,0,0,0},{0,1,1,0,0},{0,0,1,0,1},{1,0,0,0,1},{0,1,0,1,1}};
+    n = 5;
+    m = 5;
+    // cin>>n>>m;
+    memset(t,-1,sizeof(t));
+    int arr[n][m] = {{1,1,0,0,0},{0,1,1,0,0},{0,0,1,0,1},{1,0,1,0,1},{0,1,0,1,1}};
     // {{1,1,1,0,0},
     //  {0,1,1,0,0},
     //  {0,0,1,0,1},
@@ -37,9 +43,9 @@ int main(){
     //  {0,1,0,1,1}};
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
-            std::cin>>arr[i][j];
+            // std::cin>>arr[i][j];
         }
     }
-    std::cout<<FuncOne((int*)arr, n, m)<<"\n";
+    std::cout<<"The number of 1's are :: "<<FuncOne((int*)arr, n, m)<<"\n";
 
 }
